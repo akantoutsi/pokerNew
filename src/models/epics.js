b6dfdd550ce5f2d96040193887ddd30e2ff7dcfc
akantoutsi <a.kantoutsi@gmail.com>
2019-09-15 18:23:06 +0300

    installed react0redux, react-observable

---
+++ src/models/epics.js
@@ -0,0 +1,7 @@
+import { combineEpics }                                                    from 'redux-observable';
+import {  } from 'models/auth';
+import 'rxjs';
+
+export const rootEpic = combineEpics();
+
+export default rootEpic;
